2da3dca44a93f0250315f9b025cfecb9b458a37d
Harley Trung <harley@socialsci.com>
2010-12-01 01:22:33 -0500

    move local variables out to instance variables and class methods

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,26 +1,33 @@
 class Tictactoe
   def initialize
+    @board  = [[nil,nil,nil],
+               [nil,nil,nil],
+               [nil,nil,nil]]
+    @players = [:X, :O].cycle
+  end
+
+  def left_diagonal
+    [[0,0],[1,1],[2,2]]
+  end
+
+  def right_diagonal
+    [[2,0],[1,1],[0,2]]
+  end
+
+  def get_next_player
+    @players.next 
   end
 
   def start
-    board   = [[nil,nil,nil],
-               [nil,nil,nil],
-               [nil,nil,nil]]
-
-    left_diagonal = [[0,0],[1,1],[2,2]]
-    right_diagonal = [[2,0],[1,1],[0,2]]
-
-    players = [:X, :O].cycle
-
     loop do
-      current_player = players.next 
-      puts board.map { |row| row.map { |e| e || " " }.join("|") }.join("\n")
+      current_player = get_next_player
+      puts @board.map { |row| row.map { |e| e || " " }.join("|") }.join("\n")
       print "\nPlayer #{current_player}>> "
       row, col = gets.split.map { |e| e.to_i }
       puts
 
       begin
-        cell_contents = board.fetch(row).fetch(col)
+        cell_contents = @board.fetch(row).fetch(col)
       rescue IndexError
         puts "Out of bounds, try another position"
         next
@@ -31,7 +38,7 @@ class Tictactoe
         next
       end
 
-      board[row][col] = current_player
+      @board[row][col] = current_player
 
       lines = []
 
@@ -43,7 +50,7 @@ class Tictactoe
       lines << (0..2).map { |r1| [r1, col] }
 
       win = lines.any? do |line|
-        line.all? { |row,col| board[row][col] == current_player }
+        line.all? { |row,col| @board[row][col] == current_player }
       end
 
       if win
@@ -51,7 +58,7 @@ class Tictactoe
         exit
       end
 
-      if board.flatten.compact.length == 9
+      if @board.flatten.compact.length == 9
         puts "It's a draw!"
         exit
       end
