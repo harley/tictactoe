7cc0c68b1325d33155602b094cec5e23224274f0
Harley Trung <harley@socialsci.com>
2010-12-01 01:18:31 -0500

    wrap class Tictactoe around code, game start with Tictactoe.new.start

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,54 +1,60 @@
-board   = [[nil,nil,nil],
-           [nil,nil,nil],
-           [nil,nil,nil]]
-
-left_diagonal = [[0,0],[1,1],[2,2]]
-right_diagonal = [[2,0],[1,1],[0,2]]
-
-
-players = [:X, :O].cycle
-
-loop do
-  current_player = players.next 
-  puts board.map { |row| row.map { |e| e || " " }.join("|") }.join("\n")
-  print "\nPlayer #{current_player}>> "
-  row, col = gets.split.map { |e| e.to_i }
-  puts
-
-  begin
-    cell_contents = board.fetch(row).fetch(col)
-  rescue IndexError
-    puts "Out of bounds, try another position"
-    next
-  end
-  
-  if cell_contents
-    puts "Cell occupied, try another position"
-    next
+class Tictactoe
+  def initialize
   end
 
-  board[row][col] = current_player
+  def start
+    board   = [[nil,nil,nil],
+               [nil,nil,nil],
+               [nil,nil,nil]]
 
-  lines = []
+    left_diagonal = [[0,0],[1,1],[2,2]]
+    right_diagonal = [[2,0],[1,1],[0,2]]
 
-  [left_diagonal, right_diagonal].each do |line|
-    lines << line if line.include?([row,col])
-  end
+    players = [:X, :O].cycle
 
-  lines << (0..2).map { |c1| [row, c1] }
-  lines << (0..2).map { |r1| [r1, col] }
+    loop do
+      current_player = players.next 
+      puts board.map { |row| row.map { |e| e || " " }.join("|") }.join("\n")
+      print "\nPlayer #{current_player}>> "
+      row, col = gets.split.map { |e| e.to_i }
+      puts
 
-  win = lines.any? do |line|
-    line.all? { |row,col| board[row][col] == current_player }
-  end
+      begin
+        cell_contents = board.fetch(row).fetch(col)
+      rescue IndexError
+        puts "Out of bounds, try another position"
+        next
+      end
+      
+      if cell_contents
+        puts "Cell occupied, try another position"
+        next
+      end
 
-  if win
-    puts "#{current_player} wins!"
-    exit
-  end
+      board[row][col] = current_player
 
-  if board.flatten.compact.length == 9
-    puts "It's a draw!"
-    exit
+      lines = []
+
+      [left_diagonal, right_diagonal].each do |line|
+        lines << line if line.include?([row,col])
+      end
+
+      lines << (0..2).map { |c1| [row, c1] }
+      lines << (0..2).map { |r1| [r1, col] }
+
+      win = lines.any? do |line|
+        line.all? { |row,col| board[row][col] == current_player }
+      end
+
+      if win
+        puts "#{current_player} wins!"
+        exit
+      end
+
+      if board.flatten.compact.length == 9
+        puts "It's a draw!"
+        exit
+      end
+    end
   end
 end
